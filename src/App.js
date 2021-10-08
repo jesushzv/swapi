6b506d782375f1ca988dcc42207f9440c4ead354
Jesus Hector Zamora Valenzuela <jhector.zamora@hotmail.com>
2021-10-07 19:23:29 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+//CHECK THIS OUT science BITCH!
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  //2 manejo de estados
+  const [apiData, setApiData] = useState([]);
+  const [inputState, setInputState] = useState("");
+  const [starship, setStarship] = useState("Death Star");
+
+  // 1 Manejo de la api
+
+  const apiUrl = `https://swapi.dev/api/starships/?page=`;
+
+  //#3 Side effect: useEffect
+  //Manda a llamar la API y actualiza el estado solo cuando apiURL cambie
+
+  let getData = (num) => {
+    fetch(`${apiUrl}${num}`)
+      .then((res) => res.json())
+      .then((data) => setApiData((past) => [...past, ...data.results]));
+  };
+
+  useEffect(() => {
+    for (let i = 1; i <= 4; i++) {
+      getData(i);
+    }
+  }, []);
+
+
+  //Passengers functional component with a button to actualize the number of passengers
+  let Passengers = (props) => {
+    var [pas, setPas] = useState(parseInt(props.num));
+
+    return (
+      <>
+        <p>Passengers: {pas}</p>
+        <button onClick={() => setPas((past) => past + 1)}> + </button>
+      </>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(apiData)}
+
+      <h2>List of starships:</h2>
+
+      <div>
+        {apiData.map((starship) => {
+          let url = starship.url;
+          let id = url.match(/[0-9]+/);
+          id = id.join("");
+          let imageUrl = `https://starwars-visualguide.com/assets/img/starships/${id}.jpg`;
+
+          return (
+            <div>
+              <h2> {starship.name} </h2>
+              <p> Model: {starship.model} </p>
+              <p> Manufacturer: {starship.manufacturer} </p>
+              <p> Starship Class: {starship.starship_class} </p>
+
+              <section className="etiquetas">
+                <p> MGLT: {starship.MGLT} </p>
+                <p> Model: {starship.cost_in_credits} </p>
+                <p> Speed: {starship.max_atmosphering_speed} </p>
+                <img width="100" height="100" src={imageUrl} alt="spaceShipImage" />
+
+                <Passengers num={starship.passengers} />
+              </section>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
